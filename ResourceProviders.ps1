db80d1a0630023c798567da47b5dd0c980175ea2
myCloudIQ <108789047+myCloudIQ@users.noreply.github.com>
2022-08-25 06:57:31 -0400

    Update ResourceProviders.ps1

--- ResourceProviders.ps1
+++ ResourceProviders.ps1
@@ -1,2 +1,5 @@
 #Get a list of Resource Providers
 Get-AzResourceProvider -ListAvailable | Select-Object ProviderNamespace, RegistrationState
+
+#To see all Registered Resource Providers
+Get-AzResourceProvider -ListAvailable | Where-Object RegistrationState -eq "Registered" | Select-Object ProviderNamespace, RegistrationState | Sort-Object ProviderNamespace

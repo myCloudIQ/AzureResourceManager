d7ae2242e2007ee7cbaee8471db4331dc08c0f35
myCloudIQ <108789047+myCloudIQ@users.noreply.github.com>
2022-08-25 07:04:28 -0400

    Update ResourceProviders.ps1

--- ResourceProviders.ps1
+++ ResourceProviders.ps1
@@ -3,3 +3,9 @@ Get-AzResourceProvider -ListAvailable | Select-Object ProviderNamespace, Registr
 
 #To see all Registered Resource Providers
 Get-AzResourceProvider -ListAvailable | Where-Object RegistrationState -eq "Registered" | Select-Object ProviderNamespace, RegistrationState | Sort-Object ProviderNamespace
+
+#To see the resource types for a resource provider
+(Get-AzResourceProvider -ProviderNamespace Microsoft.MachineLearning).ResourceTypes.ResourceTypeName
+
+#To get the supported locations for a resource type
+((Get-AzResourceProvider -ProviderNamespace Microsoft.MachineLearning).ResourceTypes | Where-Object ResourceTypeName -eq Workspaces).Locations
